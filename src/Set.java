e84ef1d02ea566cc4abeabef5b7d19ee958e1613
Jokabi <jakobknight121@gmail.com>
2021-12-05 18:08:11 -0500

    nextAddress methods
    Added newxAddress methods to Cache.java and Set.java

--- src/Set.java
+++ src/Set.java
@@ -6,9 +6,10 @@
 public class Set {
 	int index;
 	Line[] lines;
+	int counter = 0;
 	
 	public Set(int lineNum) {
-		this.lines = new Lines[lineNum];
+		this.lines = new Line[lineNum];
 	}
 	
 	public void setIndex(int index) {
@@ -20,7 +21,32 @@ public class Set {
 	}
 	
 	public Line getLine() {
-		return Line;
+		return lines;
 	}
 
+	public void setCounter(int count) {
+		this.counter = count;
+	}
+	
+	public boolean nextAddress(int tag, int offset, char type) {
+		int lineIndex = -1;//tracks which line in the lines array is going to be modified
+		for (int i = 0; i < lines.length; i++) {
+			if(lines[i].tag == tag)
+				lineIndex = i;
+		}
+		if (lineIndex == -1) {//if there was no set found with a matching tag
+			for (int i = 0; i < lines.length; i++) {//find out which line is older
+				if (lines[i].counter < lines[lineIndex].counter) {
+					lineIndex = i;
+				}
+			}
+		}
+		lines[lineIndex].setCounter(counter);
+		boolean result = false;
+		if ((lines[lineIndex].getTag() == tag) && (lines[lineIndex].isValid))
+			result = true;
+		lines[lineIndex].setTag(tag);
+		lines[lineIndex].setOffset(offset);
+		return result;
+	}
 }
